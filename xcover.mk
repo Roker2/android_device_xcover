dadf46b8f47020cd18cdfc143129fbb791369dd6
Andrew <gold78@mail.ua>
2017-03-17 17:08:42 +0200

    Update xcover.mk

--- xcover.mk
+++ xcover.mk
@@ -8,6 +8,15 @@ LOCAL_PATH := vendor/samsung/xcover/
 
 PRODUCT_COPY_FILES += \
 $(LOCAL_PATH)/system/*:system/*
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/xcover/prebuilt/modules,system/lib/modules)
+
+PRODUCT_COPY_FILES += \
+	device/samsung/xcover/prebuilt/init.rc:root/init.rc \
+	device/samsung/xcover/prebuilt/lpm.rc:root/lpm.rc \
+	device/samsung/xcover/prebuilt/ueventd.pxa968.rc:root/ueventd.pxa968.rc
+
+PRODUCT_COPY_FILES += \
+device/samsung/xcover/prebuilt/zinitix_touch.idc:system/usr/idc/zinitix_touch.idc
 #display 320x480
 #PRODUCT_AAPT_CONFIG :=mdpi
 #PRODUCT_AAPT_PREF_CONFIG :=mdpi
