ffbd103c7e9452f651a4f806ff29b059b38c2150
Andrew <gold78@mail.ua>
2017-03-20 17:58:27 +0200

    Update xcover.mk

--- xcover.mk
+++ xcover.mk
@@ -1,13 +1,19 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
-PRODUCT_BRAND :=samsung
-PRODUCT_DEVICE :=xcover
-PRODUCT_NAME :=xcover
-#my vendor
-LOCAL_PATH := vendor/samsung/xcover/
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/system/*:system/*
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/xcover/prebuilt/modules,system/lib/modules)
 
 PRODUCT_COPY_FILES += \
@@ -16,13 +22,32 @@ PRODUCT_COPY_FILES += \
 	device/samsung/xcover/prebuilt/ueventd.pxa968.rc:root/ueventd.pxa968.rc
 
 PRODUCT_COPY_FILES += \
-device/samsung/xcover/prebuilt/zinitix_touch.idc:system/usr/idc/zinitix_touch.idc
+	device/samsung/xcover/prebuilt/zinitix_touch.idc:system/usr/idc/zinitix_touch.idc
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	ro.cwm.enable_key_repeat=true \
-ro.cwm.repeatable_keys=114,115
-#display 320x480
-#PRODUCT_AAPT_CONFIG :=mdpi
-#PRODUCT_AAPT_PREF_CONFIG :=mdpi
-#PRODUCT_CHARACTERISTICS :=phone
+PRODUCT_PACKAGES += \
+	libvmeta \
+	libvmetahal \
+	libmiscgen \
+	libcodecaacdec \
+	libcodecaacenc \
+	libcodecmp3dec \
+	libcodech263dec \
+	libcodech264dec \
+	libcodecmpeg4dec \
+	libcodecmpeg2dec \
+	libcodecwmvdec \
+	libcodecamrnbdec \
+	libcodecamrnbenc \
+	libcodecamrwbdec \
+	libcodecamrwbenc \
+	libcodecg711 \
+	libcodecjpegdec \
+	libcodecjpegenc \
+	libcodecmpeg4enc \
+	libcodech264enc \
+	libcodech263enc \
+	libcodecvmetadec \
+	libcodecvmetaenc \
+	libcodecwmadec
+
 $(call inherit-product-if-exists, vendor/samsung/xcover/xcover-vendor.mk)
