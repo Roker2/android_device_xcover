764c251d2047dafe500e4367e3c34362c5a78146
Roker2 (Dmitry Minko) <dimaminko2000@gmail.com>
2017-08-25 13:55:00 +0300

    Kernel

--- xcover.mk
+++ xcover.mk
@@ -51,3 +51,9 @@ PRODUCT_PACKAGES += \
 	libcodecwmadec
 
 $(call inherit-product-if-exists, vendor/samsung/xcover/xcover-vendor.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/samsung/xcover/prebuilt/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
